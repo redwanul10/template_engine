4881290b5e7d90ef82aa242fde4faf1f906fdf30
redwanul islam <redwanulislam12@gmail.com>
2022-01-17 16:44:55 +0600

    dynamic logo added

--- generate.js
+++ generate.js
@@ -1,24 +1,14 @@
 const fs = require("fs");
 const ejs = require("ejs");
 const express = require("express");
-const opn = require("opn");
 const app = express();
 
-app.get("/", (req, res) => {
-  fs.readFile("demo.ejs", function (err, data) {
-    const options = { title: "WELCOME TO BONGO", app: "BONGO" };
-
-    const parsedHtml = ejs.render(data.toString(), options);
-    res.send(parsedHtml);
-  });
-});
-
-app.listen(8080, (req, res) => {
-  console.log(req, res);
-});
-
 fs.readFile("demo.ejs", function (err, data) {
-  const options = { title: "WELCOME TO BONGO", app: "BONGO" };
+  const options = {
+    title: "WELCOME TO BIOSCOPE",
+    app: "BIOSCOPE",
+    logo: "./bioscope.png",
+  };
 
   const parsedHtml = ejs.render(data.toString(), options);
   fs.writeFile("index.html", parsedHtml, function (err) {
@@ -30,5 +20,3 @@ fs.readFile("demo.ejs", function (err, data) {
     console.log("");
   });
 });
-
-opn("http://localhost:8080");
